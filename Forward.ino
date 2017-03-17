c4f492dd5f7877c6d2c69284f9ec9000f03383bc
Bituhh <victor@oliveira.org.uk>
2017-03-17 19:54:57 +0000

    Changing to modify the speed.

--- Forward.ino
+++ Forward.ino
@@ -1,28 +1,22 @@
 void Forward(int16_t Speed, int16_t Distance) {
   Speed = map(constrain(Speed , 0 , 100), 0 , 100, 0, 200);
+  adjustSpeed = Speed;
+  adjustSpeed = constrain(adjustSpeed , 0 , 255);
+  distanceTo += currentDistance();
   while (Distance > currentDistance()) {
     if ((findDistance(M1Pulse) - findDistance(M2Pulse)) > errorMargin) {
-      digitalWrite(M1a , LOW);
-      digitalWrite(M1b , LOW);
-      analogWrite(PWM1 , LOW);
-      digitalWrite(M2a , HIGH);
-      digitalWrite(M2b , LOW);
-      analogWrite(PWM2 , Speed);
+      adjustSpeed--;
+      analogWrite(PWM1 , adjustSpeed);
     } else if ((findDistance(M1Pulse) - findDistance(M2Pulse)) > -errorMargin) {
-      digitalWrite(M1a , HIGH);
-      digitalWrite(M1b , LOW);
-      analogWrite(PWM1 , Speed);
-      digitalWrite(M2a , LOW);
-      digitalWrite(M2b , LOW);
-      analogWrite(PWM2 , LOW);
-    } else {
-      digitalWrite(M1a , HIGH);
-      digitalWrite(M1b , LOW);
-      analogWrite(PWM1 , Speed);
-      digitalWrite(M2a , HIGH);
-      digitalWrite(M2b , LOW);
-      analogWrite(PWM2 , Speed);
+      adjustSpeed++;
+      analogWrite(PWM1 , adjustSpeed);
     }
+    digitalWrite(M1a , HIGH);
+    digitalWrite(M1b , LOW);
+    analogWrite(PWM1 , adjustSpeed);
+    digitalWrite(M2a , HIGH);
+    digitalWrite(M2b , LOW);
+    analogWrite(PWM2 , Speed);
   }
   stopBreak();
 }
