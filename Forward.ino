a546d1aeb23b58700ceaea3e46f20e5cd8e84d3d
Bituhh <victor@oliveira.org.uk>
2017-03-17 18:56:32 +0000

    Removing else statement
    To check all wheels on the same run

--- Forward.ino
+++ Forward.ino
@@ -1,15 +1,13 @@
 void Forward(int16_t Speed, int16_t Distance) {
   Speed = map(constrain(Speed , 0 , 100), 0 , 100, 0, 200);
   while (Distance > currentDistance()) {
-    if (abs(findDistance(M1Pulse)- abs(findDistance(M2Pulse) > errorMargin) {
-      digitalWrite(M1a , HIGH);
-      digitalWrite(M1b , LOW);
-      analogWrite(PWM1 , Speed);
-      digitalWrite(M2a , LOW);
-      digitalWrite(M2b , LOW);
-      analogWrite(PWM2 , Speed);
-    }
-    else if (abs(findDistance(M2Pulse) - abs(findDistance(M1Pulse) > errorMargin) {
+    digitalWrite(M1a , HIGH);
+    digitalWrite(M1b , LOW);
+    analogWrite(PWM1 , Speed);
+    digitalWrite(M2a , HIGH);
+    digitalWrite(M2b , LOW);
+    analogWrite(PWM2 , Speed);
+    if (abs(findDistance(M1Pulse)) - abs(findDistance(M2Pulse)) > errorMargin) {
       digitalWrite(M1a , LOW);
       digitalWrite(M1b , LOW);
       analogWrite(PWM1 , Speed);
@@ -17,11 +15,11 @@ void Forward(int16_t Speed, int16_t Distance) {
       digitalWrite(M2b , LOW);
       analogWrite(PWM2 , Speed);
     }
-    else {
+    if (abs(findDistance(M2Pulse)) - abs(findDistance(M1Pulse)) > errorMargin) {
       digitalWrite(M1a , HIGH);
       digitalWrite(M1b , LOW);
       analogWrite(PWM1 , Speed);
-      digitalWrite(M2a , HIGH);
+      digitalWrite(M2a , LOW);
       digitalWrite(M2b , LOW);
       analogWrite(PWM2 , Speed);
     }
